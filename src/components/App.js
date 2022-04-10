e21c99b8c85f30a6f6c75f1749b9514c5d6c7b98
Yibizo <diegomejiasuarez@gmail.com>
2022-04-10 03:52:19 -0500

    Testing variables deleted

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,4 @@
-import { str } from "./modules.js"
 import { RecordButtons } from "./record.js";
-
-function test () {
-  return (
-    <p>This is a test</p>
-  );
-}
 
 function App() {
     return (
